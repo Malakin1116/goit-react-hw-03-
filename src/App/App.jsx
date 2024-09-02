18fc760f40f5b1dd2feb78edb5ab77cf7a2bfb21
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-09-02 16:31:11 +0300

    1.1

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -109,45 +109,47 @@ import { Formik, Form, Field } from 'formik';
 //   );
 // }
 
+
+
+import { useState} from "react";
+
+
 export default function App() {
+  const [contacts, setContacts] = useState([
+          { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+          { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+          { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+          { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+        ]
+  );
+
+
+  const [filter, setFilter] = useState("");
+
+  const deleteContact = (contactId) => {
+    setContacts((prevContacts) => {
+      return prevContacts.filter((contact) => contactId !== contact.id);
+    });
+  };
+
+  const visibleContacts = contacts.filter(
+    (contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()) ||
+      contact.number.includes(filter)
+  );
+
+  const addContact = (newContact) => {
+    setContacts((prevContacts) => {
+      return [...prevContacts, newContact];
+    });
+  };
+
   return (
-    <Formik
-      initialValues={{
-        name: "",
-        number: "",
-        search: "",
-      }}
-      onSubmit={(values, actions) =>{
-        console.log(values);
-        actions.resetForm();
-      }}
-    >
-        <Form>
-          <div>
-            <label>Name:</label>
-            <Field name="name" type="text" />
-          </div>
-          <div>
-            <label>Number:</label>
-            <Field name="number" type="text" />
-          </div>
-          <button type="submit">Add Contact</button>
-        </Form>
-      
-        <Form>
-        <div>
-            <label>search:</label>
-            <Field name="search"  type="text" />
-          </div>
-       </Form>
-      
-        {/* <ul>
-          {contactJSON.map(contact => (
-            <li key={contact.id}>
-              <Contact data={contact} />
-            </li>
-          ))}
-        </ul> */}
-    </Formik>
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onAdd={addContact} />
+      <SearchBox value={filter} onFilter={setFilter} />
+      <ContactList contacts={visibleContacts} onDelete={deleteContact} />
+    </div>
   );
 }
