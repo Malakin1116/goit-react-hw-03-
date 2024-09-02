87761f2e2db542ddc90641eb1f320ea7653e4fab
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-09-02 16:56:13 +0300

    addNewContactList

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -50,6 +50,7 @@
 import { useState} from "react";
 
 import ContactForm from "./ContactForm/ContactForm";
+import ContactList from "./ContactList/ContactList";
 
 
 export default function App() {
--- src/App/ContactList/ContactList.jsx
+++ src/App/ContactList/ContactList.jsx
@@ -16,3 +16,17 @@
 //     </ul>
 //     );
 // }
+
+import Contact from "../ContactList/Contact/Contact";
+
+export default function ContactList({ contacts, onDelete }) {
+  return (
+    <ul>
+      {contacts.map((contact) => (
+        <li key={contact.id}>
+          <Contact data={contact} onDelete={onDelete} />
+        </li>
+      ))}
+    </ul>
+  );
+}
